70188a8bbfa2275f2806eee85dabdfe81b9db101
GUSTAVO SILVA DEODATO <24122736@dsn127m1246732.cfp127aluno.local>
2025-09-23 14:35:57 -0300

    mec

--- db/script.sql
+++ db/script.sql
@@ -3,11 +3,22 @@ create database db_teajuda_aa;
 use db_teajuda_aa;
 
 create table tbl_usuario(
-	id int not null primary key auto_increment,
-    email varchar(100) not null, 
-    senha varchar(100) not null, 
-    nome_tutelado varchar (100) not null
+id int not null primary key auto_increment,
+nome varchar(100) not null,
+email varchar(100) not null,
+senha varchar(100) not null,
+nome_tutelado varchar (100) not null
 
 );
 
+insert into tbl_usuario(nome, email, senha, nome_tutelado) values(
+'iubanco',
+'back@sembanco.com',
+'bancodedados',
+'bancodedados'
+);
+
 show tables;
+
+select * from tbl_usuario order by id desc;
+
