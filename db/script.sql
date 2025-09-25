7a35e934d9794ea1ca7d537ba50a82a258de0d45
GUSTAVO SILVA DEODATO <24122736@dsn127m1246731.cfp127aluno.local>
2025-09-25 14:55:08 -0300

    git feat procedure start

--- db/script.sql
+++ db/script.sql
@@ -6,22 +6,38 @@ create table tbl_usuario(
 id int not null primary key auto_increment,
 nome varchar(100) not null,
 email varchar(100) not null,
-senha varchar(100) not null,
-nome_tutelado varchar (100) not null
+senha varchar(100) not null
 
 );
 
-insert into tbl_usuario(nome, email, senha, nome_tutelado) values(
+insert into tbl_usuario(nome, email, senha) values(
 'iubanco',
 'back@sembanco.com',
-'bancodedados',
 'bancodedados'
 );
 
 show tables;
 
+
+DELIMITER $$
+
+CREATE PROCEDURE InserirUsuario(
+    IN p_nome VARCHAR(100),
+    IN p_email VARCHAR(100),
+    IN p_senha VARCHAR(100)
+)
+BEGIN
+    INSERT INTO tbl_usuario (nome, email, senha)
+    VALUES (p_nome, p_email, p_senha);
+
+END$$
+
+DELIMITER ;
+
+
+
 select * from tbl_usuario order by id desc;
 
-ALTER TABLE tbl_usuario
-DROP COLUMN nome_tutelado;
+CALL InserirUsuario('Teste', 'teste@email.com', '1234');
 
+DROP PROCEDURE IF EXISTS InserirUsuario;
